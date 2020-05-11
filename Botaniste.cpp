1c7dd15d9d464c00be0777c97ac5ab5bd4b0707a
Yann lfrg <yann.laforge@hotmail.fr>
2020-05-11 13:37:42 +0200

    Problemes corrigé (plante carnivor ne mange pas encore)

--- Botaniste.cpp
+++ Botaniste.cpp
@@ -36,9 +36,9 @@ void Botaniste::buy(){
             break;
             case 2:
                 if(money >= 20){
-                    // // plants.push_back(new PlanteC());
-                    // money -= 20;
-                    cout << "La plante Carnivore n'est pas disponible a mon grand regret.. elles sont herbivores, qui voudrait de plante herbivores. mhpf" << endl;
+                    plants.push_back(new PlanteC());
+                    money -= 20;
+                    // cout << "La plante Carnivore n'est pas disponible a mon grand regret.. elles sont herbivores, qui voudrait de plante herbivores. mhpf" << endl;
                 }
             break;
         }
@@ -72,50 +72,53 @@ void Botaniste::takeCare(){
     cout << "Liste des plantes :" << endl;
     for (int i = 0; i < plants.size(); i++) {
         cout << "[" << i << "]" << endl;
-    }
-    cout << "Entrez le numéro de la plante a s'occuper" << endl;
-    cin >>  choicePlant;
-    while (doing == 1){
-        if(choicePlant > plants.size()){ //SOUCIS ICI DE TABLEAU MAIS PAS LE TEMPS DE LE CORRIGER....
-            cout << "Pas de plante a cette endroit" << endl;
-            choice =99;
-            doing = 0;
-        }else{
-            cout << " Fertiliser une plante [1] | Tailler une plante [2] | Arroser une plante [3] | Regarder une plante [4] | Sortir de la plantation [5]" << endl;
-            doing = 1;
-            cin >>  choice;
-            
-        }
-            switch (choice)
-            {
-            case 1:
-                if(fertilize > 0){
-                plants[choicePlant] -> fertilize(); 
-                cout << "La plante est correctement fertilise pour aujourd'hui" << endl;
-                }else{
-                    cout << "Vous n'avez pas de fertilisant" << endl;
-                }
-            break;
-            
-            case 2:
-                plants[choicePlant] -> cut();
-                cout << "Vous avez taille la plante" << endl;
-            break;
-
-            case 3: 
-                plants[choicePlant] -> water();
-                cout << "Un peu d'eau pour la plan-plante :) " << endl;
-            break;
-            case 4:
-                plants[choicePlant] -> look();
-            break;
-            case 5: 
-                cout << "Vous quittez la plantation" << endl;
+    }if(plants.size() == 0){
+         cout << "Vous n'avez pas de plantes.." << endl;
+    }else{
+        cout << "Entrez le numéro de la plante a s'occuper" << endl;
+        cin >>  choicePlant;
+        while (doing == 1){
+            if(choicePlant > plants.size()){ //SOUCIS ICI DE TABLEAU MAIS PAS LE TEMPS DE LE CORRIGER....
+                cout << "Pas de plante a cette endroit" << endl;
+                choice =99;
                 doing = 0;
-            break;
-            default:
-
-            break;    
+            }else{
+                cout << " Fertiliser une plante [1] | Tailler une plante [2] | Arroser une plante [3] | Regarder une plante [4] | Sortir de la plantation [5]" << endl;
+                doing = 1;
+                cin >>  choice;
+                
             }
+                switch (choice)
+                {
+                case 1:
+                    if(fertilize > 0){
+                    plants[choicePlant] -> fertilize(); 
+                    cout << "La plante est correctement fertilise pour aujourd'hui" << endl;
+                    }else{
+                        cout << "Vous n'avez pas de fertilisant" << endl;
+                    }
+                break;
+                
+                case 2:
+                    plants[choicePlant] -> cut();
+                    cout << "Vous avez taille la plante" << endl;
+                break;
+
+                case 3: 
+                    plants[choicePlant] -> water();
+                    cout << "Un peu d'eau pour la plan-plante :) " << endl;
+                break;
+                case 4:
+                    plants[choicePlant] -> look();
+                break;
+                case 5: 
+                    cout << "Vous quittez la plantation" << endl;
+                    doing = 0;
+                break;
+                default:
+
+                break;    
+                }
+        }
     }
 }
